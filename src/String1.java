03b87afcc6007b4f3f5f9430a7dda600e7ebcff6
Rabindra Mishra <sambhu311275@gmail.com>
2025-07-11 15:31:49 +0530

    String and its Operation in Java

---
+++ src/String1.java
@@ -0,0 +1,12 @@
+public class String1 {
+    public static void main(String[] args) {
+        String userinput = "Hello Bhaiya Kaise ho";
+        System.out.println(userinput.toLowerCase());
+        System.out.println(userinput.toUpperCase());
+
+        char index=userinput.charAt(3);
+        System.out.println("Index of character is:"+ index);
+
+        //Other instance methods includes ContainsAT();
+    }
+}
