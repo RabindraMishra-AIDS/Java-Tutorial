e6f67e30d433a9aa591296349f9e030ac6e8a52c
Rabindra Mishra <sambhu311275@gmail.com>
2025-07-12 23:11:08 +0530

    Replace method in String

---
+++ src/StringReplace.java
@@ -0,0 +1,7 @@
+public class StringReplace {
+    public static void main(String[] args){
+        String s="Hello Bhaiya Kaise Ho?";
+        String ReplaceString=s.replace("Ho?","Hai");
+        System.out.println(ReplaceString);
+    }
+}
